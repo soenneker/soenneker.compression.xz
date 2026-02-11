0718f16a3a83643f008442886ad8d957a721ebc0
Jake Soenneker <jake@soenneker.com>
2026-02-11 08:26:45 -0600

    improvements to filestream use

--- src/XZUtil.cs
+++ src/XZUtil.cs
@@ -1,17 +1,20 @@
 ﻿using SharpCompress.Compressors.Xz;
 using Soenneker.Compression.XZ.Abstract;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Soenneker.Extensions.Task;
-using System.Threading;
 
 namespace Soenneker.Compression.XZ;
 
 /// <inheritdoc cref="IXZUtil"/>
-public sealed class XZUtil: IXZUtil
+public sealed class XZUtil : IXZUtil
 {
     private readonly ILogger<XZUtil> _logger;
+
+    // 128KB is a good general-purpose streaming buffer for disk -> CPU -> disk...
+    private const int _copyBufferSize = 128 * 1024;
 
     public XZUtil(ILogger<XZUtil> logger)
     {
@@ -20,11 +23,32 @@ public sealed class XZUtil: IXZUtil
 
     public async ValueTask Decompress(string filePath, string outputFilePath, CancellationToken cancellationToken = default)
     {
+        cancellationToken.ThrowIfCancellationRequested();
+
         _logger.LogDebug("Decompressing XZ file: {XzFilePath} to {OutputFilePath} ...", filePath, outputFilePath);
 
-        await using FileStream xzInput = File.OpenRead(filePath);
-        await using var xzStream = new XZStream(xzInput);
-        await using FileStream output = File.Create(outputFilePath);
-        await xzStream.CopyToAsync(output, cancellationToken).NoSync();
+        var inputOptions = new FileStreamOptions
+        {
+            Mode = FileMode.Open,
+            Access = FileAccess.Read,
+            Share = FileShare.Read,
+            Options = FileOptions.Asynchronous | FileOptions.SequentialScan,
+            BufferSize = _copyBufferSize
+        };
+
+        var outputOptions = new FileStreamOptions
+        {
+            Mode = FileMode.Create,
+            Access = FileAccess.Write,
+            Share = FileShare.None,
+            Options = FileOptions.Asynchronous | FileOptions.SequentialScan,
+            BufferSize = _copyBufferSize
+        };
+
+        await using var input = new FileStream(filePath, inputOptions);
+        await using var xz = new XZStream(input);
+        await using var output = new FileStream(outputFilePath, outputOptions);
+
+        await xz.CopyToAsync(output, _copyBufferSize, cancellationToken).NoSync();
     }
 }
